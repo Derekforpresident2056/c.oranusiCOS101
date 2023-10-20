57283bbb0265ccc5ddff5de112456c80919eacba
Derekforpresident2056 <chukwuemeka.oranusi@pau.edu.ng>
2023-10-20 17:31:53 +0100

    (my first commit)

--- week-1/practice-1.rs
+++ week-1/practice-1.rs
@@ -1,13 +1,13 @@
 fn main(){
-println!("welcome to COS101")
+println!("welcome to COS101");
 
-println!("The course learning outcome is as follows:")
+println!("The course learning outcome is as follows:");
 
 println!("1. Distinguish between computer and computer programming,/n
           2. Develop some techniques in computer science,/n
           3. Understand the different areas of study in computer science,/n
           4. conversant with applications of compuer science,/n
           5. Navigate the career prospects in computer science,/n
-          6. conversant with computer programming concepts.")
+          6. conversant with computer programming concepts.");
 
  }         
