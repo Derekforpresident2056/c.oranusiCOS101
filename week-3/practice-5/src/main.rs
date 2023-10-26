c15ccfac9f50047c873ed9d9e0261ccc793267a8
Derekforpresident2056 <chukwuemeka.oranusi@pau.edu.ng>
2023-10-26 18:25:49 +0100

    (week 3 work)

---
+++ week-3/practice-5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@';
+    let alphabet:char = 'B';
+    let surname = "Bolaji";
+    let first_name = "Michael";
+    let middlename = "Gboyega";
+
+    println!(" ");
+    println!("special_character: {}",special_character);
+    println!("alphabet: {}",alphabet);
+    println!("name: {} {} {}",surname, middlename, first_name);
+}
